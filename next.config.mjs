03ac86a644e32bbcf1ff2b8305c630ec17bcb686
aljapri <122621599+aljapri@users.noreply.github.com>
2024-04-14 21:41:33 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https", // Protocol of the remote images
+        hostname: "img.clerk.com", // Hostname of the remote images
+        port: "", // Port of the remote images (leave empty if not using)
+        pathname: "/**", // Pathname pattern of the remote images
+      },
+    ],
+  },
+};
 
 export default nextConfig;
