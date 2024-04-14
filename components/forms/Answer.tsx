4718117b49a339c06e6e9e6b96917504d5da0473
aljapri <122621599+aljapri@users.noreply.github.com>
2024-04-14 22:10:00 +0300

    Update Answer.tsx

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -135,32 +135,44 @@ const Answer = ({ question, questionId, authorId }: Props) => {
             render={({ field }) => (
               <FormItem className="flex w-full flex-col gap-3">
                 <FormControl className="mt-3.5">
-                <Editor
-                apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
-                onInit={(evt, editor) => {
-                  // @ts-ignore
-                  editorRef.current = editor
-                }}
-                onBlur={field.onBlur}
-                onEditorChange={(content) => field.onChange(content)}
-                initialValue={parsedQuestionDetails?.content || ''}
-                init={{
-                  height: 350,
-                  menubar: false,
-                  plugins: [
-                    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview', 'anchor',
-                    'searchreplace', 'visualblocks', 'codesample', 'fullscreen',
-                    'insertdatetime', 'media', 'table'
-                  ],
-                  toolbar: 
-                  'undo redo | ' +
-                  'codesample | bold italic forecolor | alignleft aligncenter |' +
-                  'alignright alignjustify | bullist numlist',
-                  content_style: 'body { font-family:Inter; font-size:16px }',
-                  skin: mode === 'dark' ? 'oxide-dark' : 'oxide',
-                  content_css: mode === 'dark' ? 'dark' : 'light', 
-                }}
-              />
+                  <Editor
+                    apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
+                    onInit={(evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor;
+                    }}
+                    onBlur={field.onBlur}
+                    onEditorChange={(content) => field.onChange(content)}
+                    init={{
+                      height: 350,
+                      menubar: false,
+                      plugins: [
+                        "advlist",
+                        "autolink",
+                        "lists",
+                        "link",
+                        "image",
+                        "charmap",
+                        "preview",
+                        "anchor",
+                        "searchreplace",
+                        "visualblocks",
+                        "codesample",
+                        "fullscreen",
+                        "insertdatetime",
+                        "media",
+                        "table",
+                      ],
+                      toolbar:
+                        "undo redo | " +
+                        "codesample | bold italic forecolor | alignleft aligncenter |" +
+                        "alignright alignjustify | bullist numlist",
+                      content_style:
+                        "body { font-family:Inter; font-size:16px }",
+                      skin: mode === "dark" ? "oxide-dark" : "oxide",
+                      content_css: mode === "dark" ? "dark" : "light",
+                    }}
+                  />
                 </FormControl>
                 <FormMessage className="text-red-500" />
               </FormItem>
