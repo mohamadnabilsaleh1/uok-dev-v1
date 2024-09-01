f70ea9264fe75576f240519b8631802d08260093
aljapri <122621599+aljapri@users.noreply.github.com>
2024-09-01 21:16:06 +0300

    Update RenderTag.tsx

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -11,7 +11,7 @@ interface RenderTagProps {
 
 function RenderTag({_id, name, totalQuestions, showCount}: RenderTagProps) {
   return (
-    <Link to={`tags/${_id}`} key={_id} className='flex justify-between gap-2'>
+    <Link to={`/${_id}`} key={_id} className='flex justify-between gap-2'>
       <Badge className='subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase' >{name}</Badge>
 
       {showCount && (
